ae0df263dc858949f96b45a7546fb378e1a1c192
samurai1979 <xaiyax01@gmail.com>
2022-01-27 10:58:04 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,125 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="header">
+    <h1>Jignesh Patel</h1>
+    <p>Student ID: 301109873</p>
+    <p>College: Seneca</p>
+  </div>
+  <div class="body">
+    <div v-if="isLogin">
+      <p>Welcome {{name}}</p>
+      <button @click="logOut()" class="logout">Logout</button>
+    </div>
+    <div v-else>
+      <h3>Select an option</h3>
+      <button @click="logIn()" class="google-btn" v-bind:disabled="loading"><img src="./assets/google-icon.png" alt="Google Icon"> Login with Google</button>
+    </div> 
+    <div class="loader" v-if="loading">
+      <img src="./assets/loader.png" alt="loader icon" >
+    </div>
+  </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      isLogin: false,
+      name: "",
+      loading: false
+    }
+  },
+  methods: {
+    async logOut() {
+      const result = this.$gAuth.signOut();
+      if(result) {
+        this.isLogin = false
+      }
+    },
+    async logIn() {
+      this.loading = true
+      const googleUser = await this.$gAuth.signIn()
+      if(googleUser) {
+        this.isLogin = true
+        this.name = googleUser.su.qf
+        this.loading = false
+      } else {
+        this.loading = false
+      }
+    }
   }
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap');
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: "Poppins", sans-serif;
+  line-height: 100%;
+  color: #444;
+}
+.header {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+.body {
+  padding: 15px;
+  border-radius: 10px;
+  box-shadow: 0px 0px 5px #eee;
+  min-height: 300px;
+  width: 300px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border-top: 4px solid #4c8bf5;
+  border-bottom:4px solid #4c8bf5;
+}
+.body h3 {
+  text-align: center;
+}
+.google-btn {
+  height: 40px;
+  border-radius: 5px;
+  width: 250px;
+  color: #4c8bf5;
+  border: none;
+  background: #fff;
+  box-shadow: 0px 0px 4px #ddd;
+  cursor: pointer;
+  font-size: 15px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.google-btn:hover {
+  background: #eee;
+}
+.google-btn img {
+  height: 35px;
+}
+.loader img{
+  height: 50px;
+  margin: 10px 0;
+  animation: rotate 3s linear infinite;
+}
+.logout {
+  height: 40px;
+  border-radius: 5px;
+  width: 250px;
+  color: #fff;
+  border: none;
+  background: rgb(255, 27, 27);
+  font-size: 15px;
+  cursor: pointer;
+}
+@keyframes rotate {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
-import { createApp } from 'vue'
+import { createApp  } from 'vue'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+import GAuth from 'vue3-google-oauth2'
+const gauthOption = {
+  clientId: '526442939378-pd1060pd5l5p6jtqp5h6ajg94luhv6br.apps.googleusercontent.com',
+  scope: 'profile email',
+  prompt: 'select_account'
+}
+const app = createApp(App)
+app
+app.use(GAuth, gauthOption)
+app.mount('#app')
